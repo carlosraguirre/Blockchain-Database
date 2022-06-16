d592bfc89bfd09cc8adad9d38aef0834185738bd
Carlos Aguirre <craguirre952@gmail.com>
2022-06-16 13:18:38 -0400

    update seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,15 @@
-# 5.times do
-#   Block.create(timestamp: Faker::Date.in_date_period(year: 2022))
-# end
-# 5.times do
-#   Transaction.create(transaction_hash: Faker::Crypto.sha256)
-  # Transaction.create(number_of_confirmations: Faker::Number.within(range: 1..7))
-  # Transaction.create(timestamp: Faker::Date.in_date_period(year: 2022))
-  # Transaction.create(value: Faker::Number.decimal(l_digits: 4, r_digits: 2))
-  # Transaction.create(transaction_fee: Faker::Number.decimal(r_digits: 2))
-# end
-# 5.times do
-#   Address.create(address: Faker::Crypto.md5)
-#   Address.create(balance: Faker::Number.within(range: 1..99000))
+5.times do
+  block = Block.create(timestamp: Faker::Date.in_date_period(year: 2022))
+  5.times do
+    "test"
+    block.transactions.create(transaction_hash: Faker::Crypto.sha256)
+    block.transactions.create(number_of_confirmations: Faker::Number.within(range: 1..7))
+    block.transactions.create(timestamp: Faker::Date.in_date_period(year: 2022))
+    block.transactions.create(value: Faker::Number.decimal(l_digits: 4, r_digits: 2))
+    block.transactions.create(transaction_fee: Faker::Number.decimal(r_digits: 2))
+    5.times do
+      block.addresses.create(address: Faker::Crypto.md5)
+      block.addresses.create(balance: Faker::Number.within(range: 1..99000))
+    end
+  end
 end
