c937be072c19644b4d403f86b96e6c8aa46667ca
Carlos Aguirre <craguirre952@gmail.com>
2022-06-15 22:38:52 -0400

    populate seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+5.times do
+  block = Block.create(timestamp: Faker::Date.in_date_period(year: 2022))
+  5.times do
+    block.transactions.create(transaction_hash: Faker::Crypto.sha256),
+    block.transactions.create(number_of_confirmations: Faker::Number.within(range: 1..7)),
+    block.transactions.create(timestamp: Faker::Date.in_date_period(year: 2022)),
+    block.transactions.create(value: Faker::Number.decimal (l_digits: 4, r_digits: 2)),
+    block.transactions.create(transaction_fee: Faker::Number.decimal(r_digits: 2)),
+    block.addresses.create(address: Faker::Crypto.md5),
+    block.addresses.create(balance: Faker::Number.decimal (l_digits: 5, r_digits:2))
+  end
+end
